539578dc5b7605ac059d96798c8df658fdcf5ec7
ice soup <port.harbor.icesoup@gmail.com>
2022-08-02 16:20:04 +0900

    components #1 Reactコンポーネントのサンプル構成を導入

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//@ts-ignore
+import App from './Components/App';
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
