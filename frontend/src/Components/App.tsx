13e2ad4d2837fb8d4a7f0c5a415dfaf310660094
ice soup <port.harbor.icesoup@gmail.com>
2022-08-06 05:49:14 +0900

    components #1 初期ページにサンプルコンポーネントを表示する処理を作成

---
+++ frontend/src/Components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import FCTemplate from './FCTemplate'
+
+const App : React.FunctionComponent = () => {
+
+  return (
+    <div>
+      <h1>Hello</h1>
+      <FCTemplate sampleProp="test"/>
+    </div>
+  );
+
+};
+
+
+export default App;
