98ca9b064ad041f708f9864b25ee8529e6dce7b1
ice soup <port.harbor.icesoup@gmail.com>
2022-08-06 06:54:03 +0900

    comonents #1 ページ切り替え処理の基本構成を作成

--- frontend/src/Components/App.tsx
+++ frontend/src/Components/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import FCTemplate from './FCTemplate'
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom'
+import { Grid } from '@material-ui/core';
+import { EditorPage } from './Pages/EditorPage'
+import { ContactPage } from './Pages/ContactPage'
+import { SettingPage } from './Pages/SettingPage'
 
 const App : React.FunctionComponent = () => {
 
@@ -7,6 +12,46 @@ const App : React.FunctionComponent = () => {
     <div>
       <h1>Hello</h1>
       <FCTemplate sampleProp="test"/>
+
+      <BrowserRouter>
+
+        <div className = 'Header'>
+          <Grid container>
+
+            <Grid item  xs = { 3 } sm = { 8 }></Grid>
+
+            <Grid item  xs = { 3 } sm = { 1 }>
+              <Link to='/editor' style={ { textDecoration: 'none' , fontWeight: 'bold' } }>
+                <span>Editor</span>
+              </Link>
+            </Grid>
+
+            <Grid item  xs = { 3 } sm = { 1 }>
+              <Link to='/setting' style={ { textDecoration: 'none' , fontWeight: 'bold' } }>
+                <span>Setting</span>
+              </Link>
+            </Grid>
+
+            <Grid item  xs = { 3 } sm = { 1 }>
+              <Link to='/setting' style={ { textDecoration: 'none' , fontWeight: 'bold' } }>
+                <span>Setting</span>
+              </Link>
+            </Grid>
+
+          </Grid>
+        </div>
+
+        <div className = 'Main'>
+          <Routes>
+            <Route path = '/'        element = { <EditorPage />} />
+            <Route path = '/editor'  element = { <EditorPage />} />
+            <Route path = '/setting' element = { <SettingPage />} />
+            <Route path = '/contact' element = { <ContactPage />} />
+          </Routes>
+        </div>
+
+      </BrowserRouter>
+
     </div>
   );
 
