51461fa4ec3b44b0566dc174de70fe0495db6943
ice soup <port.harbor.icesoup@gmail.com>
2022-08-10 01:44:45 +0900

    components #2 Editorページのレイアウトを作成

---
+++ frontend/src/Components/Organisms/PlaybackScreen.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState, useRef } from "react";
+import * as THREE from 'three';
+
+/**
+ * Summary	: ジェネラティブアート作品を再生するComponent
+ * Logic		: - 作品ファイルの読み込みおよび復号化を行う
+ *            - 復号化した情報をレンダリング用データへ加工する
+ *            - 親ComponentからCCCを受け取り、DDDとしたものを子Componentに渡す
+ * View			: - XXXする
+ */
+
+interface Props {
+  sampleProp ?: any;
+}
+
+export const PlaybackScreen: React.FC<Props> = ({ sampleProp }) => {
+
+  type ScreenSize = { width: number, height: number }
+
+  // ___ state ___ ___ ___ ___ ___
+  const [ sampleState,  setSampleState ]  = useState<string>('This is SampleState');
+  const [ screenSize,   setScreenSize ]   = useState<ScreenSize>({ width: 960, height: 540});
+
+  // ___ use effect ___ ___ ___ ___ ___
+  useEffect( () => { console.log(sampleState) }, [ sampleState ] );
+  useEffect( () => { render() }, [] );    // DOMの描画後（canvas要素が生成後）にThree.jsのレンダリングを行う必要があるため、useEffectに
+  
+  // ___ event handler ___ ___ ___ ___ ___
+  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+  };
+
+  // ___ method ___ ___ ___ ___ ___
+  const test = () => {
+    console.log('test');
+  }
+
+  const render = () => {
+
+    const renderer: any = new THREE.WebGLRenderer({
+      canvas: document.querySelector("#canvas") as HTMLCanvasElement
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(screenSize.width, screenSize.height);
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(45, screenSize.width / screenSize.height);
+    camera.position.set(0, 0, +1000);
+    const geometry = new THREE.BoxGeometry(400, 400, 400);
+    const material = new THREE.MeshNormalMaterial();
+    const box = new THREE.Mesh(geometry, material);
+
+    scene.add(box);
+
+    const tick = () => {
+      box.rotation.y += 0.01;
+      renderer.render(scene, camera);
+      requestAnimationFrame(tick);
+    }
+
+    tick();
+    
+  };
+
+  return (
+    <div>
+      <canvas id='canvas' />
+    </div>
+  );
+
+};
+
+export default PlaybackScreen
+
+
+
+
+// 表示エリアのサイズを取得する処理のサンプル
+const Sample = () => {
+
+  const ref: any | null = useRef(null);
+
+  useEffect(() => {
+    console.log(ref.current);
+    console.log(
+      JSON.stringify(ref.current.getBoundingClientRect())
+    );
+  }, []);
+
+  return (
+    <div ref={ref}>
+      <h2> Sample </h2>
+    </div>
+  );
+};
--- frontend/src/Components/Pages/EditorPage.tsx
+++ frontend/src/Components/Pages/EditorPage.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
-import * as THREE from 'three';
+import React, { useEffect, useState, useRef } from "react";
+import { PlaybackScreen } from '../Organisms/PlaybackScreen'
+
 
 type Props = {
   sampleProp ?: any;
@@ -12,7 +13,6 @@ export const EditorPage : React.FC<Props> = ({ sampleProp }) => {
 
   // ___ use effect ___ ___ ___ ___ ___
   useEffect( () => { console.log(sampleState) }, [ sampleState ] );
-  useEffect( () => { render() }, [] );   // didMountで描画しないと、Cannot read property 'width' of nullというエラーが出る
   
   // ___ event handler ___ ___ ___ ___ ___
   const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
@@ -23,46 +23,49 @@ export const EditorPage : React.FC<Props> = ({ sampleProp }) => {
     console.log('test');
   }
 
-
-  const render = () => {
-    
-    const tick = () => {
-      box.rotation.y += 0.01;
-      renderer.render(scene, camera);
-      requestAnimationFrame(tick);
-    }
-
-    const width = 960;
-    const height = 540;
-
-    const renderer: any = new THREE.WebGLRenderer({
-      canvas: document.querySelector("#canvas") as HTMLCanvasElement
-    });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(width, height);
-
-    const scene = new THREE.Scene();
-
-    const camera = new THREE.PerspectiveCamera(45, width / height);
-    camera.position.set(0, 0, +1000);
-
-    const geometry = new THREE.BoxGeometry(400, 400, 400);
-    const material = new THREE.MeshNormalMaterial();
-    const box = new THREE.Mesh(geometry, material);
-    scene.add(box);
-
-    tick();
-
-  };
-
-
-
-  return (
+  return(
     <div>
-      <h2>{ EditorPage.name }</h2>
-      <canvas id='canvas' />
+      <SwitchPanel />
+      <PlaybackScreen />
     </div>
   );
 };
 
+
 export default EditorPage
+
+
+
+const SwitchPanel = () => {
+  
+  return(
+    <div>
+      <button>Works</button>
+      <button>Edit</button>
+      <button>Preview</button>
+    </div>
+  )
+
+}
+
+
+
+
+// 表示エリアのサイズを取得する処理のサンプル
+const Sample = () => {
+
+  const ref: any | null = useRef(null);
+
+  useEffect(() => {
+    console.log(ref.current);
+    console.log(
+      JSON.stringify(ref.current.getBoundingClientRect())
+    );
+  }, []);
+
+  return (
+    <div ref={ref}>
+      <h2> Sample </h2>
+    </div>
+  );
+};
