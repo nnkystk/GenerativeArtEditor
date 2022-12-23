392dc8221aec568a17bae4f9fb47770b13adfc71
ice soup <port.harbor.icesoup@gmail.com>
2022-12-23 10:03:46 +0900

    GAEEngine #22 フォルダ構成を決定

---
+++ frontend/src/GAEEngine/Interfaces/ITDModel.ts
@@ -0,0 +1,18 @@
+export interface ITDModel{
+  
+  id        : any;
+  tdObj     : any;
+  effects   : any;
+  property  : any;
+}
+
+/**
+ * 
+ * id            : TDModelID;
+ * tdObj         : any;
+ * effectStorage : EffectModelStorage;
+ * property      : TDModelProperty;
+ * 
+ */
+
+export default ITDModel
---
+++ frontend/src/GAEEngine/WorkComponents/TDModels/TDModelForTHREE.ts
@@ -0,0 +1,11 @@
+export class Sample{
+  
+  sampleProp : string | number;
+
+  constructor(sampleProp: string | number){
+    this.sampleProp = sampleProp;
+  }
+
+}
+
+export default Sample
